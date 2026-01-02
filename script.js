08593637162694eba9df19730b4bfc49318d2923
Shynee1 <jackbarkerford@gmail.com>
2026-01-02 01:16:47 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let activeSection = null;
+
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        
+        if (activeSection) {
+            activeSection.classList.remove('expanded');
+        }
+
+        targetSection.classList.add('expanded');
+        activeSection = targetSection;
+        
+        targetSection.scrollIntoView();
+    });
+});
+
+
+window.addEventListener('scroll', () => {
+    if (activeSection && !checkVisible(activeSection)) {
+        activeSection.classList.remove('expanded');
+        activeSection = null;
+    }
+});
+
+
+function checkVisible(elm) {
+  const rect = elm.getBoundingClientRect();
+  const viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+  return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+}
